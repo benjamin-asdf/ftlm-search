eb3afed8cbaa3a73560d8a50a7dcb7ae1b223369
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2023-08-19 19:45:52 +0200

    Add config

--- publish.sh
+++ publish.sh
@@ -3,3 +3,4 @@
 rsync -avz run.sh  linode:/var/www/ftlm-search/
 rsync -avz target linode:/var/www/ftlm-search/
 rsync -avz release.jar linode:/var/www/ftlm-search/
+rsync -avz prod-config.edn linode:/var/www/ftlm-search/config.edn
